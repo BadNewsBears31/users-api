d67c862879b616f4da50f5faa9eed9ae1d2bff34
Aidan Ramos <ajramos-gonsalves@myseneca.ca>
2025-12-05 14:59:34 -0500

    fix5

--- user-service.js
+++ user-service.js
@@ -32,31 +32,36 @@ module.exports.connect = function () {
 };
 
 module.exports.registerUser = function (userData) {
-    return new Promise(function (resolve, reject) {
-
-        if (userData.password != userData.password2) {
-            reject("Passwords do not match");
-        } else {
-
-            bcrypt.hash(userData.password, 10).then(hash => {
-
-                userData.password = hash;
-
-                let newUser = new User(userData);
-
-                newUser.save().then(() => {
-                    resolve("User " + userData.userName + " successfully registered");  
-                }).catch(err => {
-                    if (err.code == 11000) {
-                        reject("User Name already taken");
-                    } else {
-                        reject("There was an error creating the user: " + err);
-                    }
-                })
-            }).catch(err => reject(err));
-        }
+    return new Promise((resolve, reject) => {
+      if (!userData.userName) {
+        return reject("User name is required");
+      }
+  
+      if (userData.password !== userData.password2) {
+        return reject("Passwords do not match");
+      }
+  
+      bcrypt.hash(userData.password, 10)
+        .then(hash => {
+          userData.password = hash;
+          let newUser = new User(userData);
+  
+          newUser.save()
+            .then(() => {
+              resolve(`User ${userData.userName} successfully registered`);
+            })
+            .catch(err => {
+              if (err.code === 11000) {
+                reject("User Name already taken");
+              } else {
+                reject("There was an error creating the user: " + err);
+              }
+            });
+        })
+        .catch(err => reject("Error hashing password: " + err));
     });
 };
+  
 
 module.exports.checkUser = function (userData) {
     return new Promise(function (resolve, reject) {
